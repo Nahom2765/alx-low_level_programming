17acc270bf0ad250f4eab1f676a5396e222e57f1
Nahom2765 <19nahoma@gmail.com>
2022-05-04 06:47:47 -0700

    ffs work

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,18 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _putchar(char c)
+{ 
+printf("please work..,\n");
+
+return (0);
+}
